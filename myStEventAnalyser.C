8d820b111e5d76c3756bebf87d48088dd5ccc97d
Irakli Chakaberia <iraklic@rcas6007.rcf.bnl.gov>
2018-09-11 16:03:33 -0400

    fixed some typos and added option to run onlt one event

--- myStEventAnalyser.C
+++ myStEventAnalyser.C
@@ -1,40 +1,32 @@
-void myStEventAnalyser(const char * file) {
-	TFile * outRoot = new TFile("testOut.root", "RECREATE");
+void myStEventAnalyser(const char * file, const int eventToSelect = -1) {
+	cout << file << " to be analyzed" << endl;
+	cout << eventToSelect << " to be selected" << endl;
 
 	FILE * outFile;
-	FILE * outFile_iTPC;
-	FILE * outFile_TPC;
 	char outFileName[100];
-	char outFileName_iTPC[100];
-	char outFileName_TPC[100];
 
 //	const char * file = "st_cosmic_adc_19053068_raw_2000015.event.root";
 	gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
 	loadSharedLibraries();
 
 	gROOT->LoadMacro("bfc.C");
-	TString Chain("StEvent, nodefault, mysql, in");
-//	TString Chain("StEvent, nodefault, mysql, in, quiet");
+//	TString Chain("StEvent, nodefault, mysql, in");
+	TString Chain("StEvent, nodefault, mysql, in, quiet");
 	bfc(0, Chain, file); // This will make chain
 
-	for (int event = 0; event < 10000000; event++) {
+//	LOOP OVER EVENTS
+	for (int event = 0; event < 200000; event++) {
 		int iMake = chain->MakeEvent();
 		if (iMake % 10 == kStEOF || iMake % 10==kStFatal) break;
 
 		StEvent * pEvent = (StEvent*) chain->GetInputDS("StEvent");
-		if (event % 1000 == 0) cout << pEvent->id() << endl;
-//		if (pEvent->id() > 53000 && pEvent->id() < 54000) cout << "Event: Run "<< pEvent->runId() << " Event No: " << pEvent->id() << endl;
-//		if (pEvent->id() != 194093) continue;
-//
-//		if (pEvent->id() != 2420421) {
-//			cout << pEvent->id() << endl;
-//			continue; // JUST AN EVENT SELECTOR, REMOVE WHEN NOT NEEDED
-//		}
+		if (event % 1000 == 0) cout << "Working on event " << event << " -=- * * * Event: Run "<< pEvent->runId() << " Event No: " << pEvent->id() << " * * * -=-" << endl;
 
-		else cout << "Event: Run "<< pEvent->runId() << " Event No: " << pEvent->id() << endl;
-		sprintf(outFileName, "StEvent_%d_%d.csv", pEvent->runId(),  pEvent->id());
-		sprintf(outFileName_iTPC, "StEvent_%d_%d_iTPC.json", pEvent->runId(),  pEvent->id());
-		sprintf(outFileName_TPC, "StEvent_%d_%d_TPC.json", pEvent->runId(),  pEvent->id());
+		if (eventToSelect != -1 && pEvent->id() != eventToSelect) continue; // event selector condition
+
+		cout << " -=- * * * Event: Run "<< pEvent->runId() << " Event No: " << pEvent->id() << " * * * -=-" << endl;
+		sprintf(outFileName, "StEvent_%d_%d.json", pEvent->runId(),  pEvent->id());
+		outFile = fopen(outFileName, "w");
 
 		StTpcHitCollection* TpcHitCollection = pEvent->tpcHitCollection();
 		if (!TpcHitCollection) {
@@ -42,6 +34,13 @@ void myStEventAnalyser(const char * file) {
 			return;
 		}
 		unsigned int numberOfSectors = TpcHitCollection->numberOfSectors();
+
+//		ADDING HEADER TO THE OUTPUT json FILE
+		fprintf(outFile, "{\"EVENT\": {\"R\": %d, \"Evt\": %d, \"B\": 0.5, \"tm\": 1528087733},", pEvent->runId(), pEvent->id());
+		fprintf(outFile, "\"META\": {\n\"HITS\": {\"TPC\": {\"type\": \"3D\", \"options\": {\"size\": 5, \"color\": 100255}}},\n\"TRACKS\": {\"type\": \"3D\", \"tracks\": {\"size\":5, \"r_min\": 0, \"r_max\": 2000 }}\n},\n");
+		fprintf(outFile, "\"HITS\": {\"TPC\": [\n");
+		
+//		BELOW IS MY iTPC 2018 RELATED CHECKS THAT CAN BE REMOVED IF NOT NEEDED ==============================
 		StTpcSectorHitCollection* sectorCollection = TpcHitCollection->sector(19); //Get sector 20
 
 		unsigned int numberOfPadrows = sectorCollection->numberOfPadrows();
@@ -65,29 +64,28 @@ void myStEventAnalyser(const char * file) {
 			}
 		} // Loop over rows in sector 20
 
-//		cout << "\t NoHits = " << NoHits << endl;
-//		cout << "\t NoBadHits = " << NoBadHits << endl;
-		if (NoHits < 20) {
-			cout << "Less then 20 hits in sector 20 : "  << NoHits << endl;
-			continue;
-		}
+//		if (NoHits < 20) {
+//			cout << "Less then 20 hits in sector 20 : "  << NoHits << endl;
+//			continue;
+//		}
+
+//		=====================================================================================================
 
 		StSPtrVecTrackNode & trackNode = pEvent->trackNodes();
 		int nTracks = trackNode.size();
-		if (nTracks < 20) continue; // removing hits from short tracks that are usually not used in the analysis
+//		if (nTracks < 20) continue; // removing hits from short tracks that are usually not used in the analysis
+
 		StTrackNode * node = 0;
 		for (int track = 0; track < nTracks; track++) {
 			node = trackNode[track];
 			if (!node) continue;
+
+#if 0
+//			PRIMARY TRACKS
 			StPrimaryTrack* pTrack = static_cast<StPrimaryTrack*>(node->track(primary));
 			if (!pTrack) continue;
-//			cout << *gTrack << endl;
 			if (! pTrack->detectorInfo()) {cout << "=============== detectorInfo is missing" << endl; continue;}
 			StPtrVecHit phvec = pTrack->detectorInfo()->hits();
-
-//			if (hvec.size() < 70) continue;
-
-//			if (track != 113 && track != 514 && track != 559) continue; 
 
 			cout << "Event: Run "<< pEvent->runId() << " Event No: " << pEvent->id() << endl;
 			cout << "Track number : " << track << endl;
@@ -96,15 +94,9 @@ void myStEventAnalyser(const char * file) {
 //				if (hvec[hit]->detector() == kTpcId) {
 					StTpcHit *tpcHit = static_cast<StTpcHit *> (phvec[hit]);
 
-//					if (!tpcHit || tpcHit->sector() != 20) continue;
 					if (!tpcHit) continue;
 					if (tpcHit->flag() != 0) continue;
-					if(!doOnce) {
-						outFile = fopen(outFileName, "w");
-						outFile_iTPC = fopen(outFileName_iTPC, "w");
-						outFile_TPC = fopen(outFileName_TPC, "w");
-						doOnce = true;
-					}
+
 					float hitX = tpcHit->position().x();
 					float hitY = tpcHit->position().y();
 					float hitZ = tpcHit->position().z();
@@ -112,14 +104,20 @@ void myStEventAnalyser(const char * file) {
 //					if (hitZ > 0) continue;
 //					if (tpcHit->padrow() > 40) fprintf(outFile_TPC, "[%.2f, %.2f, %.2f],\n", hitX, hitY, hitZ);
 //					else fprintf(outFile_iTPC, "[%.2f, %.2f, %.2f],\n", hitX, hitY, hitZ);
+//
 					fprintf(outFile, "[%.2f, %.2f, %.2f],\n", hitX, hitY, hitZ);
+
+
 //					cout << "[" << hitX << ", " << hitY << ", " << hitZ << "]," << endl;
 //					fprintf(outFile, "%d, %d, %d, %d, %f, %f, %f, %f\n", track, tpcHit->pad(), tpcHit->padrow(), tpcHit->timeBucket(), tpcHit->adc(), hitX, hitY, hitZ);
 //				}
 			}
-/*			
+#endif
+
+#if 1
+//			GLOBAL TRACKS
 			StGlobalTrack* gTrack = static_cast<StGlobalTrack*>(node->track(global));
-			if (!pTrack) continue;
+			if (!gTrack) continue;
 			if (! gTrack->detectorInfo()) {cout << "=============== detectorInfo is missing" << endl; continue;}
 			StPtrVecHit ghvec = gTrack->detectorInfo()->hits();
 			for (int hit = 0; hit < ghvec.size(); hit++) {
@@ -147,13 +145,10 @@ void myStEventAnalyser(const char * file) {
 //					fprintf(outFile, "%d, %d, %d, %d, %f, %f, %f, %f\n", track, tpcHit->pad(), tpcHit->padrow(), tpcHit->timeBucket(), tpcHit->adc(), hitX, hitY, hitZ);
 //				}
 			}
-*/		
+#endif		
 
 		}
-		if (doOnce) {
-			fclose(outFile);
-			fclose(outFile_iTPC);
-			fclose(outFile_TPC);
-		}
+		fprintf(outFile, "[0,0,0]]\n}\n}");
+		fclose(outFile);
 	} // Event Loop
 }
